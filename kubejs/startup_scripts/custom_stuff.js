10f8f20edf07e5d8b5afa5ad55edfe377404a799
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-07-28 22:46:05 +0800

    [T] kubejs startup scripts (#5)
    Co-authored-by: 海福 <18917570841@163.com>
    Co-authored-by: 北巳 <1321583590@qq.com>

--- kubejs/startup_scripts/custom_stuff.js
+++ kubejs/startup_scripts/custom_stuff.js
@@ -5,30 +5,30 @@ console.info('Hello, World! (You will only see this line once in console, during
 // New Items
 onEvent('item.registry', event => {
 
-event.create('skyopolis_additions:nether_token').displayName('Nether Token').glow(true)
-event.create('skyopolis_additions:end_token').displayName('End Token').glow(true)
-event.create('skyopolis_additions:undergarden_token').displayName('Undergarden Token').glow(true)
-event.create('skyopolis_additions:space_token').displayName('Space Token').glow(true)
-event.create('skyopolis_additions:blank_token').displayName('Blank Token')
+event.create('skyopolis_additions:nether_token').displayName('下界通行证').glow(true)
+event.create('skyopolis_additions:end_token').displayName('末地通行证').glow(true)
+event.create('skyopolis_additions:undergarden_token').displayName('深园通行证').glow(true)
+event.create('skyopolis_additions:space_token').displayName('深空通行证').glow(true)
+event.create('skyopolis_additions:blank_token').displayName('空白通行证')
 
-event.create('skyopolis_additions:sky_bucks').displayName('Sky Bucks')
-event.create('skyopolis_additions:star').displayName('Star')
+event.create('skyopolis_additions:sky_bucks').displayName('天空币')
+event.create('skyopolis_additions:star').displayName('星星')
 
-event.create('skyopolis_additions:weak_enriching_water_catalyst').displayName('Weak Enriching Water Catalyst')
-event.create('skyopolis_additions:eroding_water_catalyst').displayName('Eroding Water Catalyst')
-event.create('skyopolis_additions:strong_enriching_water_catalyst').displayName('Strong Enriching Water Catalyst')
-event.create('skyopolis_additions:undergarden_enriching_water_catalyst').displayName('Undergarden Enriching Water Catalyst')
-event.create('skyopolis_additions:nether_enriching_water_catalyst').displayName('Nether Enriching Water Catalyst')
-event.create('skyopolis_additions:cosmos_enriching_water_catalyst').displayName('Cosmos Enriching Water Catalyst')
-event.create('skyopolis_additions:ultimate_enriching_water_catalyst').displayName('Ultimate Enriching Water Catalyst')
+event.create('skyopolis_additions:weak_enriching_water_catalyst').displayName('弱富集水催化剂')
+event.create('skyopolis_additions:eroding_water_catalyst').displayName('侵蚀水催化剂')
+event.create('skyopolis_additions:strong_enriching_water_catalyst').displayName('强富集水催化剂')
+event.create('skyopolis_additions:undergarden_enriching_water_catalyst').displayName('深园富集水催化剂')
+event.create('skyopolis_additions:nether_enriching_water_catalyst').displayName('下界富集水催化剂')
+event.create('skyopolis_additions:cosmos_enriching_water_catalyst').displayName('宇宙富集水催化剂')
+event.create('skyopolis_additions:ultimate_enriching_water_catalyst').displayName('最终富集水催化剂')
 
-event.create('skyopolis_additions:blank_bucket').displayName('Blank Bucket')
-event.create('skyopolis_additions:blank_ingot').displayName('Blank Ingot')
+event.create('skyopolis_additions:blank_bucket').displayName('空白桶')
+event.create('skyopolis_additions:blank_ingot').displayName('空白锭')
 
-event.create('skyopolis_additions:silver_ingot').displayName('Silver Ingot')
-event.create('skyopolis_additions:silver_nugget').displayName('Silver Nugget')
+event.create('skyopolis_additions:silver_ingot').displayName('银锭')
+event.create('skyopolis_additions:silver_nugget').displayName('银粒')
 
-event.create('skyopolis_additions:skyblock_menu').displayName('Skyblock Menu').texture(`minecraft:item/paper`).color(0, 0x2986CC).maxStackSize(1)
+event.create('skyopolis_additions:skyblock_menu').displayName('空岛菜单').texture(`minecraft:item/paper`).color(0, 0x2986CC).maxStackSize(1)
 
 
 
@@ -54,7 +54,7 @@ onEvent('block.modification', event => {
 
 onEvent('block.registry', event => {
 
-event.create('skyopolis:blank_ore').displayName('Blank Ore').material('stone').hardness(1.0).requiresTool(false)
+event.create('skyopolis:blank_ore').displayName('空白矿石').material('stone').hardness(1.0).requiresTool(false)
 
 })
 
@@ -62,13 +62,13 @@ event.create('skyopolis:blank_ore').displayName('Blank Ore').material('stone').h
 
 
 onEvent('fluid.registry', event => {
-  event.create('skyopolis:undergarden_enriching_water').displayName('Undergarden Enriching Water').thickTexture(0x3C4C34).bucketColor(0x3C4C34)
-  event.create('skyopolis:weak_enriching_water').displayName('Weak Enriching Water').thickTexture(0xD79113).bucketColor(0xD79113)
-  event.create('skyopolis:strong_enriching_water').displayName('Strong Enriching Water').thickTexture(0x808080).bucketColor(0x808080)
-  event.create('skyopolis:nether_enriching_water').displayName('Nether Enriching Water').thickTexture(0x580704).bucketColor(0x580704)
-  event.create('skyopolis:cosmos_enriching_water').displayName('Cosmos Enriching Water').thickTexture(0x323232).bucketColor(0x323232)
-  event.create('skyopolis:eroding_water').displayName('Eroding Water').thickTexture(0x392C4C).bucketColor(0x392C4C)
-  event.create('skyopolis:ultimate_enriching_water').displayName('Ultimate Enriching Water').thickTexture(0xFC99E7).bucketColor(0xFC99E7)
+  event.create('skyopolis:undergarden_enriching_water').displayName('深园富集水').thickTexture(0x3C4C34).bucketColor(0x3C4C34)
+  event.create('skyopolis:weak_enriching_water').displayName('弱富集水').thickTexture(0xD79113).bucketColor(0xD79113)
+  event.create('skyopolis:strong_enriching_water').displayName('强富集水').thickTexture(0x808080).bucketColor(0x808080)
+  event.create('skyopolis:nether_enriching_water').displayName('下界富集水').thickTexture(0x580704).bucketColor(0x580704)
+  event.create('skyopolis:cosmos_enriching_water').displayName('宇宙富集水').thickTexture(0x323232).bucketColor(0x323232)
+  event.create('skyopolis:eroding_water').displayName('侵蚀水').thickTexture(0x392C4C).bucketColor(0x392C4C)
+  event.create('skyopolis:ultimate_enriching_water').displayName('最终富集水').thickTexture(0xFC99E7).bucketColor(0xFC99E7)
 
 })
 
